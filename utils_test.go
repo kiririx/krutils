35b1b4aad71489250798323d1f73c54ca70a2531
キリリ <kiriri_ri@126.com>
2022-06-23 14:54:10 +0800

    feat: add algorithm

--- utils_test.go
+++ utils_test.go
@@ -1,9 +1,10 @@
 package krutils
 
 import (
+	"github.com/kiririx/krutils/str_util"
 	"testing"
 )
 
 func TestConv(t *testing.T) {
-	t.Log(StrUtil.ToStr(1))
+	t.Log(str_util.ToStr(make(chan int)))
 }
