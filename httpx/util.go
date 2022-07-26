08b5e633b43754fd945fdf652a2b3c823f2ea7fa
キリリ <kiriri_ri@126.com>
2022-07-26 15:26:02 +0800

    feat: add http query

--- httpx/util.go
+++ httpx/util.go
@@ -20,3 +20,44 @@ func GetURLWithQuery[V common.BaseType](url string, query map[string]V) string {
 	}
 	return url
 }
+
+func GetQueryParam(url string, key string) string {
+	if strings.Contains(url, "?") {
+		url = url[strings.Index(url, "?")+1:]
+		var kvs []string
+		if strings.Contains(url, "&") {
+			kvs = strings.Split(url, "&")
+		} else {
+			kvs = []string{url}
+		}
+		for _, kv := range kvs {
+			if strings.Contains(kv, "=") {
+				if strings.Split(kv, "=")[0] == key {
+					return strings.Split(kv, "=")[1]
+				}
+			}
+		}
+	}
+	return ""
+}
+
+func GetQueryParams(url string) map[string]string {
+	params := make(map[string]string)
+	if strings.Contains(url, "?") {
+		url = url[strings.Index(url, "?")+1:]
+		var kvs []string
+		if strings.Contains(url, "&") {
+			kvs = strings.Split(url, "&")
+		} else {
+			kvs = []string{url}
+		}
+		for _, kv := range kvs {
+			if strings.Contains(kv, "=") {
+				params[strings.Split(kv, "=")[0]] = strings.Split(kv, "=")[1]
+			} else {
+				params[kv] = ""
+			}
+		}
+	}
+	return params
+}
